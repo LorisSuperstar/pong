64cb82476317a08a797b824b868d1ad470375def
LorisSuperstar <180442737+LorisSuperstar@users.noreply.github.com>
2026-02-11 10:50:11 +0100

    Add Start Button

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::sync::WaitTimeoutResult;
+
+slint::include_modules!();
+
 fn main() {
-    println!("Hello, world!");
+    let ui = MainWindow::new().unwrap();
+
+    let weak = ui.as_weak();
+
+    let x_border_left: f32 = 0.0;
+    let y_border_upp: f32 = 0.0;
+    let x_border_right: f32 = 1450.0;
+    let y_border_bottom: f32 = 950.0;
+
+    ui.on_button_clicked(move || {
+        let ui:MainWindow = weak.upgrade().unwrap();
+       
+    });
+    
+
+    ui.run().unwrap();
 }
